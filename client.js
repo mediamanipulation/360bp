77299a43a7e0bc104a5c8ab0eec416b5c7ae6f87
mediamanipulation <manipulation2000@hotmail.com>
2019-07-27 19:02:40 -0500

    commit 1

--- client.js
+++ client.js
@@ -20,7 +20,7 @@ function init(bundle, parent, options = {}) {
     myFlatSurface0.setAngle(-Math.PI /1.3, 0);
 
     const myFlatSurface1 = new Surface(600, 400, Surface.SurfaceShape.Flat);
-    myFlatSurface1.setAngle(-Math.PI / 3.8, 0);
+    myFlatSurface1.setAngle(-Math.PI / 4, 0);
 
     const myFlatSurface2 = new Surface(600, 400, Surface.SurfaceShape.Flat);
     myFlatSurface2.setAngle(-Math.PI /-3.5, 0);
@@ -35,7 +35,7 @@ function init(bundle, parent, options = {}) {
         }),
         myCylinderSurface
     );
-
+ 
     r360.renderToSurface(
         r360.createRoot('Flag0', {
             image: 'target.png'
--- components/Flag0.js
+++ components/Flag0.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { AppRegistry, asset, StyleSheet, Image } from 'react-360';
+import { AppRegistry, asset, StyleSheet, Image, Text, View } from 'react-360';
 
 export default class Flag0 extends React.Component {
     render() {
         const { flag } = styles;
-
-        return <Image style={flag} source={asset(this.props.image)} />;
+        const { txt } = styles;
+        return(
+        <View style={target} > 
+          <Text>button 1</Text>
+          <Image  source={asset(this.props.image)} />
+        </View>
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    flag: {
+    target: {
         height: 400,
         width: 600
     }
+  
 });
